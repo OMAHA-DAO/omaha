22d29c452405cdd867816b2c228f57f6f0ba4bda
Denis Vesnin <ancient.animated@gmail.com>
2022-05-25 15:21:01 +0300

    webpack upgrade

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,16 +1,115 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const WebpackOnBuildPlugin = require('on-build-webpack2');
+var HtmlWebpackSkipAssetsPlugin = require('html-webpack-skip-assets-plugin').HtmlWebpackSkipAssetsPlugin;
+
+const glob = require("glob");
+const fs = require('fs');
+const path = require('path');
+const argv = require('yargs').argv;
+
+const root = path.resolve( path.resolve( __dirname+"/..") );
+const isDev = argv.mode === 'development';
+const isProd = !isDev;
+
+let files = {
+    'bundle.js' : './src/script.js',
+    'css/styles': './src/styles.css',
+};
+
+const optimization = () => {
+    const config = {
+        // splitChunks: {
+        //     cacheGroups: {
+        //         vendors: {
+        //             test: /[\\/]node_modules[\\/]|[\\/]js[\\/]includes[\\/]/,
+        //             chunks: 'all',
+        //             minSize: 999999999,
+        //
+        //             name(module, chunks, cacheGroupKey) {
+        //                 let name = chunks.map((item) => item.name).join('~');
+        //                 return cacheGroupKey+"/"+name;
+        //             },
+        //         },
+        //     }
+        //
+        // },
+    };
+
+    if (isProd) {
+        config.minimizer = [
+            new UglifyJsPlugin({
+                test: /\.js($|\?)/i,
+            })
+        ]
+    }
+
+    return config
+};
+
+const babelOptions = preset => {
+    const opts = {
+        presets: [
+            '@babel/preset-env'
+        ],
+        plugins:[
+            '@babel/proposal-class-properties',
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-transform-typescript',
+        ]
+    };
+
+    if (preset) {
+        opts.presets.push(preset)
+    }
+
+    return opts
+};
+
+function del_file_folder(path_prod,extension){
+    path_prod = path_prod.replaceAll('\\', '/');
+
+    let unlinked = [];
+    glob(`${path_prod}/**/*.${extension}`, function (er, files) {
+
+        files.forEach(file => {
+            fs.unlinkSync(path.resolve(file));
+            unlinked.push(file);
+        })
+
+    });
+
+    if (unlinked.length > 0) {
+        console.log("================");
+        console.log('Removed files: ', unlinked );
+        console.log("================");
+    }
+
+}
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script.js'),
-    output:
-    {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
+    entry: files,
+    output: {
+        path: root + '/dist/',
+        filename: "[name].js",
+        chunkFilename: 'dist/modules/[name].[chunkhash].js',
+        publicPath: "/"
+        //sourceMapFilename: '[name].js.map'
     },
-    devtool: 'source-map',
+
+    //entry: path.resolve(__dirname, '../src/script.js'),
+    // output:
+    // {
+    //     filename: 'bundle.[contenthash].js',
+    //     path: path.resolve(__dirname, '../dist')
+    // },
+
+    mode: argv.mode,
+    devtool: isDev === true ?'inline-source-map' : false,
+    optimization: optimization(),
     plugins:
     [
         new CopyWebpackPlugin({
@@ -20,9 +119,20 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
+            minify: true,
+            //excludeAssets: [/css\/.*js/],
         }),
-        new MiniCSSExtractPlugin()
+        new HtmlWebpackSkipAssetsPlugin({
+            excludeAssets: [/css\/.*js/]
+        }),
+
+        new MiniCSSExtractPlugin({
+            filename: "[name].css",
+        }),
+
+        new WebpackOnBuildPlugin(async function(stats) {
+            await del_file_folder(root+"/dist/css","js");
+        }),
     ],
     module:
     {
@@ -34,14 +144,47 @@ module.exports = {
                 use: ['html-loader']
             },
 
-            // JS
+            // JS (MAIN)
             {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [/*'@babel/plugin-syntax-jsx', */'@babel/proposal-class-properties', '@babel/plugin-proposal-object-rest-spread','@babel/plugin-syntax-dynamic-import',
+                            // Set import path replaces (e.g. ~/ -> root)
+                            // [
+                            //     'babel-plugin-root-import',
+                            //     {
+                            //         "paths": [
+                            //             {
+                            //                 "rootPathSuffix": root+"/",
+                            //                 "rootPathPrefix": "~/"
+                            //             },
+                            //             {
+                            //                 "rootPathSuffix": root+"/resources/",
+                            //                 "rootPathPrefix": "resources/"
+                            //             },
+                            //         ]
+                            //     }
+                            // ]
+                        ]
+                    }
+                }
+            },
+            {   // JS
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
+                use: ["source-map-loader"],
+                enforce: "pre"
+            },
+            {   // JSX
+                test: /\.(jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: babelOptions('@babel/preset-react')
+                }
             },
 
             // CSS
@@ -49,8 +192,31 @@ module.exports = {
                 test: /\.css$/,
                 use:
                 [
+                    'style-loader',
                     MiniCSSExtractPlugin.loader,
-                    'css-loader'
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            url: false
+                        }
+
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                sourceMap: true,
+                                plugins: [
+                                    "autoprefixer",
+                                    ["css-mqpacker", {
+                                        sort: true
+                                    }],
+                                    "cssnano"
+                                ]
+                            }
+                        }
+                    }
                 ]
             },
 
@@ -85,4 +251,4 @@ module.exports = {
             }
         ]
     }
-}
+};
