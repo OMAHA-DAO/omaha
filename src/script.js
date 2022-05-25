3bcdf69e4083b83831041ca45f7e58be3712b34a
Vlad-Ch <vladchuvikov1@gmail.com>
2022-05-26 02:29:27 +0300

    fix front

--- src/script.js
+++ src/script.js
@@ -744,3 +744,51 @@ for (const [key, value] of Object.entries(models)) {
 })()
 
 import "./includes/milestones_animation";
+
+Promise.all([
+	import(
+		/* webpackChunkName: "splide" */
+		`@splidejs/splide`
+		),
+]).then(function (modules) {
+	console.log("[IMPORT]", modules);
+
+	// Expand modules into variables for more convenient use
+	const [
+		splideModule,
+	] = modules;
+	window.Splide = splideModule.default;
+
+	/* Slider Products */
+	let postSlider = {
+		speed: 400,
+		autoWidth: true,
+		// perPage: 1,       // Ignored if autoWidth: true, but need for lazy
+		pagination: false,
+		arrows: false,
+		drag: 'free',
+		flickPower: 300,
+		lazyLoad: 'nearby',
+		preloadPages: 2
+	};
+
+	new window.Splide( '.posts-list', postSlider).mount();
+
+});
+
+if(window.innerWidth < 768){
+
+	$(window).scroll(function () {
+
+		var scroll = $(window).scrollTop() + $(window).height();
+		var offset = $(".cube-section").offset().top;
+
+		if (scroll > offset) {
+			$('.ANI-main-3').fadeOut();
+		} else {
+			$('.ANI-main-3').fadeIn();
+		}
+
+	});
+
+}
