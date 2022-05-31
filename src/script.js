96b1282da4faa301f49d33f5902d1e5939d8629b
Denis Vesnin <ancient.animated@gmail.com>
2022-05-31 11:56:14 +0300

    merging...

--- src/script.js
+++ src/script.js
@@ -168,7 +168,7 @@ const models=Object.create({
                             })
                             .add({
                                 targets:document.querySelector('.ANI-main-3'),
-                                translateY:['-100%',0],  easing,  duration:700,
+                                translateY:['-120%',0],  easing,  duration:700,
                             })
                         }
                     })
@@ -330,3 +330,53 @@ const models=Object.create({
         }
         animate()
 })();
+
+import "./includes/milestones_animation";
+
+Promise.all([
+    import(
+        /* webpackChunkName: "splide" */
+        `@splidejs/splide`
+        ),
+]).then(function (modules) {
+    console.log("[IMPORT]", modules);
+
+    // Expand modules into variables for more convenient use
+    const [
+        splideModule,
+    ] = modules;
+    window.Splide = splideModule.default;
+
+    /* Slider Products */
+    let postSlider = {
+        speed: 400,
+        autoWidth: true,
+        // perPage: 1,       // Ignored if autoWidth: true, but need for lazy
+        pagination: false,
+        arrows: false,
+        drag: 'free',
+        flickPower: 300,
+        lazyLoad: 'nearby',
+        preloadPages: 2
+    };
+
+    new window.Splide( '.posts-list', postSlider).mount();
+
+});
+
+if(window.innerWidth < 768){
+
+    $(window).scroll(function () {
+
+        var scroll = $(window).scrollTop() + $(window).height();
+        var offset = $(".cube-section").offset().top;
+
+        if (scroll > offset) {
+            $('.ANI-main-3').fadeOut();
+        } else {
+            $('.ANI-main-3').fadeIn();
+        }
+
+    });
+
+}
